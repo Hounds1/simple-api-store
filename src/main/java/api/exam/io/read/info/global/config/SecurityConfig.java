f41f7123305016e0be293d16a12d77512849af3c
Hounds1 <zoodisx@gmail.com>
2023-04-12 11:23:27 +0900

    SecurityConfig 일부 수정

--- src/main/java/api/exam/io/read/info/global/config/SecurityConfig.java
+++ src/main/java/api/exam/io/read/info/global/config/SecurityConfig.java
@@ -11,11 +11,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -40,13 +41,14 @@ public class SecurityConfig {
     }
 
     @Bean
-    public void configure(WebSecurity web) {
-        web.ignoring()
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web
+                .ignoring()
                 .antMatchers("/js/**", "/favicon.ico/**", "/css/**", "/index", "/docs/**");
     }
 
     @Bean
-    public void configure(HttpSecurity security) throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity security) throws Exception{
         security.csrf().disable()
                 .cors().configurationSource(corsConfigurationSource())
                 .and()
@@ -70,6 +72,8 @@ public class SecurityConfig {
 
         security
                 .formLogin().disable();
+
+        return security.build();
     }
 
 
